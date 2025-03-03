7eeb6866bbfddd2f0e04a42a194d566a14f5b95f
LlanderSei <seiranruroku@gmail.com>
2025-03-03 21:03:57 +0800

    Added functionality to register and login.

--- project_folder/assets/js/script.js
+++ project_folder/assets/js/script.js
@@ -9,3 +9,74 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
   container.classList.remove('active');
 });
+
+function loadUsers() {
+  const usersJson = localStorage.getItem('users');
+  return usersJson ? JSON.parse(usersJson) : [];
+}
+
+function saveUsers(users) {
+  localStorage.setItem('users', JSON.stringify(users));
+}
+
+function register() {
+  const name = document.getElementById('name').value;
+  const username = document.getElementById('username').value;
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  const confirmPassword = document.getElementById('confirm-password').value;
+
+  if (!name || !username || !email || !password || !confirmPassword) {
+    alert('Please fill in all fields');
+    return;
+  }
+
+  if (!email.includes('@')) {
+    alert('Invalid email, require @');
+    return;
+  }
+
+  if (password !== confirmPassword) {
+    alert('Passwords do not match');
+    return;
+  }
+
+  const users = loadUsers();
+  const existingUser = users.find(user => user.username === username);
+  if (existingUser) {
+    alert('Username already exists');
+    return;
+  }
+
+  const newUser = { name, username, email, password };
+  users.push(newUser);
+  saveUsers(users);
+  alert('You are registered');
+  document.getElementById('name').value = '';
+  document.getElementById('username').value = '';
+  document.getElementById('email').value = '';
+  document.getElementById('password').value = '';
+  document.getElementById('confirm-password').value = '';
+  container.classList.remove('active');
+}
+
+// Login
+function login() {
+  const username = document.getElementById('login-username').value;
+  const password = document.getElementById('login-password').value;
+
+  const users = loadUsers();
+  const user = users.find(user => user.username === username && user.password === password);
+
+  if (user) {
+    window.location.href = "../project_folder/index.html";
+    alert('Login successful');
+    document.getElementById('login-username').value = '';
+    document.getElementById('login-password').value = '';
+  } else {
+    alert('Incorrect username or password');
+  }
+}
+
+document.getElementById('regbtn').addEventListener('click', register);
+document.getElementById('loginbtn').addEventListener('click', login);
